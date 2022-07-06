893fe2555eb0cda798e546e596fcbc8104170305
Yesid Cano <yesidcanoc@gmail.com>
2022-07-06 14:57:50 +0200

    added __str__

--- tbcore/models.py
+++ tbcore/models.py
@@ -1,20 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+# Create your models here
 
 
 class Plan (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)  #usually something like course title
+    plan_name = models.CharField(max_length=100)  #usually something like course title
+
+    def __str__(self):
+        return self.plan_name
 
 class Category (models.Model):
-    name = models.CharField(max_length=100) # there are a total of 8 categories: hallway chatter, organization etc.
+    category_name = models.CharField(max_length=100) # there are a total of 8 categories: hallway chatter, organization etc.
     description = models.TextField()
+    def __str__(self):
+        return self.category_name
 
 class OnlineIdea (models.Model):
-    name = models.CharField (max_length= 200)
+    idea_name = models.CharField (max_length= 200)
     description = models.TextField()
     short_description = models.CharField(max_length= 200) # used for checklist
+
+    def __str__(self):
+        return self.idea_name
+
 
 class CategoryOnlineIdea (models.Model):
     category = models.ManyToManyField(Category)
