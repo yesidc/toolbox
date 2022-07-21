223277716ed82019c264df1ecfb4bb06efb12e36
Yesid Cano <yesidcanoc@gmail.com>
2022-07-21 19:16:09 +0200

    added start_page

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tbcore.urls')),
     path('plan/', include('plan.urls')),
 ]
