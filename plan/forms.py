84540ec04392dc603ad70beb7fea09a0058466f4
Yesid Cano <yesidcanoc@gmail.com>
2022-09-18 22:32:57 +0200

    change form on idea_detail page

--- plan/forms.py
+++ plan/forms.py
@@ -5,9 +5,9 @@ class NotesForm (ModelForm):
     class Meta:
         model= Notes
         fields= ['idea_note']
-        help_texts = {
-            'idea_note': ('Some useful help text.'),
-        }
+        # help_texts = {
+        #     'idea_note': ('Some useful help text.'),
+        # }
 
 class PlanForm (ModelForm):
     class Meta:
