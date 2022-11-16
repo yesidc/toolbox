5042052dcdb730f25b9211b406c6e09bf9ff29d6
Yesid Cano <yesidcanoc@gmail.com>
2022-11-16 12:54:22 +0100

    added inclusion tag show_idea_property

--- tbcore/templatetags/toolbox_custom_tags.py
+++ tbcore/templatetags/toolbox_custom_tags.py
@@ -86,6 +86,13 @@ def show_ideas(context,user_authenticated):
     }
 
 
+@register.inclusion_tag('plan/show_idea_property.html')
+def show_idea_property(idea_property,property_name):
+    return {
+        'idea_property': idea_property,
+        'property_name': property_name
+    }
+
 @register.simple_tag()
 def md_to_html(value):
     return markdown.markdown(value)
