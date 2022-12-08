14579ac36db61c6a93f99bbb0b7cf2d01a371d1b
Yesid Cano <yesidcanoc@gmail.com>
2022-12-08 10:47:57 +0100

    update

--- plan/static/plan/js/utils.js
+++ plan/static/plan/js/utils.js
@@ -3,8 +3,7 @@ const all_plans = document.getElementsByClassName('all-plan-sidebar')
 const plan_name_dom = document.getElementById('plan_name_dom')
 
 
-
-function open_internal_link(internal_url){
+function open_internal_link(internal_url) {
 
     //var link_internal = window.location.host + internal_url
     window.open(internal_url)
@@ -12,26 +11,26 @@ function open_internal_link(internal_url){
 }
 
 
-try{
-    document.getElementById('plan-side-bar-'+add_hyphen(plan_name_dom.dataset.planName)).style.backgroundColor='#e6e6ff'
-}catch (error){
+try {
+    document.getElementById('plan-side-bar-' + add_hyphen(plan_name_dom.dataset.planName)).style.backgroundColor = '#e6e6ff'
+} catch (error) {
 
 }
 
 
-function add_hyphen(plan){
+function add_hyphen(plan) {
     return plan.replaceAll(' ', '-');
 }
 
-function set_plan_collapse_color (){
-    for(p of plan_collapse){
-        p.style.backgroundColor='white'
+function set_plan_collapse_color() {
+    for (p of plan_collapse) {
+        p.style.backgroundColor = 'white'
     }
 }
 
-function set_menu_color(){
-    for (i of all_plans){
-        i.style.backgroundColor='white'
+function set_menu_color() {
+    for (i of all_plans) {
+        i.style.backgroundColor = 'white'
     }
 }
 
@@ -49,41 +48,44 @@ for (i of plan_collapse) {
                     plan_id: event.target.getAttribute('data-plan-id')
                 },
                 success: function (response) {
+                    try {
+                        $('#plan_name_dom').text(response.plan_name_ajax)
+                        // plan's name color
+                        set_menu_color()
+                        set_plan_collapse_color()
 
-                    $('#plan_name_dom').text(response.plan_name_ajax)
-                    // plan's name color
-                    set_menu_color()
-                    set_plan_collapse_color()
+                        // sets the color of the plan's name on the sidebar
+                        document.getElementById('plan-side-bar-' + add_hyphen(response.plan_name_ajax)).style.backgroundColor = '#e6e6ff'
+                        // sets the color of the drop-down menu on the sidebar
+                        document.getElementById(add_hyphen(response.plan_name_ajax)).style.backgroundColor = '#e6e6ff'
 
-                    // sets the color of the plan's name on the sidebar
-                    document.getElementById('plan-side-bar-'+add_hyphen(response.plan_name_ajax)).style.backgroundColor='#e6e6ff'
-                    // sets the color of the drop-down menu on the sidebar
-                    document.getElementById(add_hyphen(response.plan_name_ajax)).style.backgroundColor='#e6e6ff'
+                        for (const c_done of response.category_ready) {
+                            //ticks off the (sidebar)checkbox if user has already selected at least one idea for any given category
+                            document.getElementById(c_done + response.plan_id_response).checked = true
+                        }
 
-                    for (const c_done of response.category_ready){
-                        //ticks off the (sidebar)checkbox if user has already selected at least one idea for any given category
-                        document.getElementById(c_done+response.plan_id_response ).checked = true
-                    }
+                        // when the user selects a plan (using the left-navigation bar); the checkboxes on the block_content page are updated accordingly/dynamically
 
-                    // when the user selects a plan (using the left-navigation bar); the checkboxes on the block_content page are updated accordingly/dynamically
-
-                    try{
                         let idea_container = document.getElementById('online-idea-container')
-                        let request_new_template = new XMLHttpRequest();
-                        request_new_template.open('GET', window.location.pathname+'?mode=update');
-                        request_new_template.onload = function () {
-                            // the response is the rendered HTML
+                        if (idea_container !== null) {
 
-                            let myHTML = request_new_template.response;
+                            let request_new_template = new XMLHttpRequest();
+                            request_new_template.open('GET', window.location.pathname + '?mode=update');
+                            request_new_template.onload = function () {
+                                // the response is the rendered HTML
 
-                            // templated HTML returned by the server (managed by update_selected_idea view.)
-                            idea_container.innerHTML = myHTML;
+                                let myHTML = request_new_template.response;
 
-                        };
-                        request_new_template.send();
+                                // templated HTML returned by the server (managed by update_selected_idea view.)
+                                idea_container.innerHTML = myHTML;
 
-                    }catch (error){
+                            };
+                            request_new_template.send();
+                        }
 
+
+                    } catch (error) {
+                        console.log('Error caught successfully')
                     }
 
 
@@ -102,8 +104,8 @@ function delete_plan(obj) {
     let plan_name = obj.getAttribute('data-object-name');
     let plan_id = obj.getAttribute('data-object-id');
     const ask = confirm('Do you want to delete: ' + plan_name);
-    if (ask){
-        document.location.href = "/delete_plan"+ "/"+plan_id+"/"
+    if (ask) {
+        document.location.href = "/delete_plan" + "/" + plan_id + "/"
         //document.location.href = "{% url 'delete_plan'%}"+ "/"+plan_id
 
     }
